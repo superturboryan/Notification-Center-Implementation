cebd2c0117c1b7f72eacf5f0fc27dc4dddfad02f
Ryan Forsyth <forsyth.r@gmail.com>
2023-09-21 14:07:11 +0100

    Implement NotifCenter, NotifName, connect to SwiftUI example view

--- Notification Center Implementation/ContentView.swift
+++ Notification Center Implementation/ContentView.swift
@@ -5,17 +5,59 @@
 //  Created by Ryan Forsyth on 2023-09-21.
 //
 
+import Combine
 import SwiftUI
 
+extension NotifCenter.NotifName {
+    static let Increment1 = Self.init(rawValue: "increment1")
+    static let Increment2 = Self.init(rawValue: "increment2")
+    static let Decrement = Self.init(rawValue: "decrement")
+}
+
 struct ContentView: View {
+    
+    @State var counter1 = 0
+    @State var counter2 = 0
+    @State var decrement = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text("Count: \(counter1)")
+            Button("Increment by 1") {
+                NotifCenter.default.postNotification(.Increment1)
+            }
+            Spacer()
+            Text("Count: \(counter2)")
+            Button("Increment by 2") {
+                NotifCenter.default.postNotification(.Increment2)
+            }
+            Spacer()
+            HStack {
+                Text("Decrement amount:")
+                Picker("\(decrement)", selection: $decrement) {
+                    ForEach(1...5, id: \.self) { value in
+                        Text("\(value)")
+                    }
+                }
+            }
+            Button("Decrement all by \(decrement)") {
+                NotifCenter.default.postNotification(.Decrement, decrement)
+            }
+            Spacer()
         }
-        .padding()
+        .onReceive(NotifCenter.default.publisher(.Increment1), perform: { _ in
+            counter1 += 1
+        })
+        .onReceive(NotifCenter.default.publisher(.Increment2), perform: { _ in
+            counter2 += 2
+        })
+        .onReceive(NotifCenter.default.publisher(.Decrement), perform: { value in
+            if let value = value as? Int {
+                counter1 -= value
+                counter2 -= value
+            }
+        })
     }
 }
 
---
+++ Notification Center Implementation/NotifCenter.swift
@@ -0,0 +1,51 @@
+//
+//  NotifCenter.swift
+//  Notification Center Implementation
+//
+//  Created by Ryan Forsyth on 2023-09-21.
+//
+
+import Combine
+import Foundation
+
+class NotifCenter {
+    static let `default` = NotifCenter()
+    
+    private var publishers = [NotifName : PassthroughSubject<Any?, Never>]()
+    private var subscriptions = Set<AnyCancellable>()
+    
+    func publisher(_ name: NotifName) -> AnyPublisher<Any?, Never> {
+        if publishers[name] == nil {
+            publishers[name] = PassthroughSubject<Any?, Never>()
+        }
+        return publishers
+            .first(where: { $0.key == name })!.value
+            .eraseToAnyPublisher()
+    }
+    
+    func postNotification(_ name: NotifName, _ object: Any? = nil) {
+        guard let publisher = publishers[name] else {
+            return
+        }
+        publisher.send(object)
+    }
+    
+    func addObserver(_ observer: AnyObject, selector: Selector, name: NotifName, object: Any?) {
+        if publishers[name] == nil {
+            publishers[name] = PassthroughSubject<Any?, Never>()
+        }
+        let publisher = publishers[name]!
+        publisher.sink { optionalValue in
+            _ = observer.perform(selector, with: object)
+        }.store(in: &subscriptions)
+    }
+}
+
+extension NotifCenter {
+    struct NotifName: Hashable, RawRepresentable {
+        var rawValue: String
+        init(rawValue: String) {
+            self.rawValue = rawValue
+        }
+    }
+}
