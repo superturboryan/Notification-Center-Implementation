c9acc719bc5ea888f0fdcea9b513c3109d626943
Ryan Forsyth <forsyth.r@gmail.com>
2023-09-21 12:25:58 +0100

    Initial Commit

---
+++ Notification Center Implementation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notification Center Implementation
+//
+//  Created by Ryan Forsyth on 2023-09-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notification Center Implementation/Notification_Center_ImplementationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notification_Center_ImplementationApp.swift
+//  Notification Center Implementation
+//
+//  Created by Ryan Forsyth on 2023-09-21.
+//
+
+import SwiftUI
+
+@main
+struct Notification_Center_ImplementationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
